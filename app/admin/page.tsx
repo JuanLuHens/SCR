8baca1be00cf4089842d8ef5b47ff07d60274be6
Juanlu <juanlu82@gmail.com>
2025-04-21 17:36:10 +0200

    add admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,6 @@ export default async function AdminPage() {
   // Esta página es solo un placeholder para un futuro panel de administración
   // En una implementación real, aquí verificaríamos la autenticación del administrador
 
-  // Redirigir a la página principal por ahora
+  // Redirigir a la página principal por ahora aa
   redirect("/")
 }
